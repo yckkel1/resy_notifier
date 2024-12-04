e628e12d008c1145f959a0002dd550ee8b562a5b
yckkel1 <sammwang21@gmail.com>
2024-12-03 21:53:02 -0500

    Add DatabaseManager and testings

--- src/resy_notifier/main.py
+++ src/resy_notifier/main.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 from api_client import ResyAPIClient
 import os
+from db_manager import DatabaseManager
 
 load_dotenv()
 
 def main():
-    api_key = os.getenv("RESY_API_KEY")
+    db_manager = DatabaseManager()
+    api_key = db_manager.get_active_api_key()
     base_url = os.getenv("BASE_URL")
     client = ResyAPIClient(api_key, base_url)
     print(client.get_availability(834))
